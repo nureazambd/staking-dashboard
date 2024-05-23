210bfe40e06794a5d8043622b49a728afa07e182
nureazambd <nur.azam.bd@gmail.com>
2024-05-23 16:25:07 +0600

    overview section button and card added

--- packages/page-staking/src/Validators/Summary copy.tsx
+++ packages/page-staking/src/Validators/Summary copy.tsx
@@ -27,7 +27,7 @@ function Summary ({ className = '', stakingOverview, targets: { counterForNomina
   return (
     <StyledSummaryBox className={className}>
       {/* <span style={{ background:"#2d292d"}}> */}
-      {/* validators waiting active / nominators */}
+
       <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
   <div className="p-6 flex">
   <CardSummary label={t('validators')}>
@@ -46,7 +46,7 @@ function Summary ({ className = '', stakingOverview, targets: { counterForNomina
             : <span className='--tmp'>99</span>
           }
         </CardSummary>
-
+        
         <CardSummary
           className='media--1000'
           label={
@@ -70,48 +70,56 @@ function Summary ({ className = '', stakingOverview, targets: { counterForNomina
         </CardSummary>
   </div>
 </div>
-
-
-
-
-
-
-<a href="#_" className="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
-<span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
-<span className="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
-<span className="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
-<span className="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
-</span>
-{(idealStake > 0) && Number.isFinite(idealStake) && (
-          <CardSummary
-            className='media--1400'
-            label={t('ideal staked')}
-          >
-            <>{(idealStake * 100).toFixed(1)}{percent}</>
-          </CardSummary>
-        )}
-        {(stakedFraction > 0) && (
-          <CardSummary
-            className='media--1300'
-            label={t('staked')}
-          >
-            <>{(stakedFraction * 100).toFixed(1)}{percent}</>
-          </CardSummary>
-        )}
-        {(inflation > 0) && Number.isFinite(inflation) && (
-          <CardSummary
-            className='media--1200'
-            label={t('inflation')}
-          >
-            <>{inflation.toFixed(1)}{percent}</>
-          </CardSummary>
-        )}
-</a>
-
-
-      
+      <section style={{ background:"#C0C0C0", color:"#000000", borderRadius:"10px", padding:"25px"}}>
+        {/* <CardSummary label={t('validators')}> */}
+        <CardSummary label={t('validators')}>
+          {stakingOverview
+            ? <>{formatNumber(stakingOverview.validators.length)}&nbsp;/&nbsp;{formatNumber(stakingOverview.validatorCount)}</>
+            : <span className='--tmp'>999 / 999</span>
+          }
+        </CardSummary>
+        {/* <div>
+          <label>validators</label>
+          {stakingOverview
+            ? <>{formatNumber(stakingOverview.validators.length)}&nbsp;/&nbsp;{formatNumber(stakingOverview.validatorCount)}</>
+            : <span className='--tmp'>999 / 999</span>
+          }
+        </div> */}
+        
+        <CardSummary
+          className='media--900'
+          label={t('waiting')}
+        >
+          {waitingIds
+            ? formatNumber(waitingIds.length)
+            : <span className='--tmp'>99</span>
+          }
+        </CardSummary>
+        <CardSummary
+          className='media--1000'
+          label={
+            counterForNominators
+              ? t('active / nominators')
+              : t('nominators')
+          }
+        >
+          
+          {nominators
+            ? (
+              <>
+                {formatNumber(nominators.length)}
+                {counterForNominators && (
+                  <>&nbsp;/&nbsp;{formatNumber(counterForNominators)}</>
+                )}
+              </>
+            )
+            : <span className='--tmp'>999 / 999</span>
+          }
+        </CardSummary>
+        
+      </section>
       {/* </span> */}
-      {/* <section style={{ background:"#C0C0C0", color:"#000000", borderRadius:"10px", padding:"25px"}}>
+      <section style={{ background:"#C0C0C0", color:"#000000", borderRadius:"10px", padding:"25px"}}>
         {(idealStake > 0) && Number.isFinite(idealStake) && (
           <CardSummary
             className='media--1400'
@@ -136,7 +144,7 @@ function Summary ({ className = '', stakingOverview, targets: { counterForNomina
             <>{inflation.toFixed(1)}{percent}</>
           </CardSummary>
         )}
-      </section> */}
+      </section>
       <section style={{ background:"#C0C0C0", color:"#000000", borderRadius:"10px", padding:"25px"}}>
         <SummarySession />
       </section>
